b74fb7f6f08ece4a065db82120ac1378f4e9c031
thefireKS <KS29092003@gmail.com>
2024-10-24 20:22:58 +0300

    UPD: updated few svg's; Made bg music louder

--- scripts/MainMusicPlayer.gd
+++ scripts/MainMusicPlayer.gd
@@ -6,6 +6,6 @@ func _ready():
 	main_theme = AudioStreamPlayer.new()
 	add_child(main_theme)
 	main_theme.stream = load("res://sfx/music_theme.wav")
-	main_theme.volume_db = -40
+	main_theme.volume_db = -25
 	main_theme.play()
 	print("should play")
